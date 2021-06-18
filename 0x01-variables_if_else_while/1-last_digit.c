e5eac8a4dc5d2867330942a58f1c901594fd7828
paulacarvani <3312@holbertonschool.com>
2021-06-18 15:49:48 -0700

    New changes in the betty style and the order of variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 int n;
-int last = (n % 10);
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
+int last = (n % 10);
 
 if (last > 5)
 {
@@ -22,7 +23,7 @@ else if (last == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, last);
 }
-else
+else if (last < 6 && last != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 }
