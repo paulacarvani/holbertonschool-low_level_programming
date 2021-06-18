0d0a9d4ea3956b04f138d6eebd5142ae8916a5fd
paulacarvani <3312@holbertonschool.com>
2021-06-18 15:23:16 -0700

    Updates script with betty style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,17 +9,16 @@
 int main(void)
 {
 int n;
+int last = (n % 10);
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
-int last = (n % 10);
 
 if (last > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
-else if (last = 0)
+else if (last == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, last);
 }
