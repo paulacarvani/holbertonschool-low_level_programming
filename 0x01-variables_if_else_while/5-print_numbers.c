3f70c3029cd00901d1e3f8211558368496464726
paulacarvani <3312@holbertonschool.com>
2021-06-18 20:04:30 -0700

    New program that prints the numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int numbers = 48;
+while (numbers <= 59)
+{
+putchar (numbers);
+numbers++;
+}
+{
+putchar ('\n');
+}
+return (0);
+}
