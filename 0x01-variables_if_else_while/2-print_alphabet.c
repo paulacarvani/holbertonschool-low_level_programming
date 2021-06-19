9fe39d45d5ce9afb25dded286302d8b1cddf6198
paulacarvani <3312@holbertonschool.com>
2021-06-18 18:26:04 -0700

    Creates a script for the function putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int alphabet = 97;
+
+while (alphabet <= 122)
+
+{
+putchar (alphabet);
+alphabet++;
+}
+{
+putchar('\n');
+}
+return (0);
+}
