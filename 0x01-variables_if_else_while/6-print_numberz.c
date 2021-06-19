a18be82777eb0322e2e03aa840208e77157ca962
paulacarvani <3312@holbertonschool.com>
2021-06-18 20:11:35 -0700

    Program with no char variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int numberz = 48;
+while (numberz <= 57)
+{
+putchar (numberz);
+numberz++;
+}
+{
+putchar ('\n');
+}
+return (0);
+}
