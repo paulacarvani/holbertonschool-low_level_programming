1a32222f3c4d168a32c8d8e293217ce2f46312c9
paulacarvani <3312@holbertonschool.com>
2021-06-18 19:34:15 -0700

    Updates the betty style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,18 @@
  */
 int main(void)
 {
-int alphabt;
-alphabt > 97;
-alphabt != 101 && alphabt != 113;
-
+int alphabt = 97;
+/*
+ *alphabt > 97;
+ *alphabt != 101 && alphabt != 113;
+ */
 while (alphabt <= 122)
+{
+if (alphabt != 101 && alphabt != 113)
 {
 putchar (alphabt);
 alphabt++;
+}
 }
 {putchar ('\n');
 }
