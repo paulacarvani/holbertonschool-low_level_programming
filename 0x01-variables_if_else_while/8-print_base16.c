5ac8ca0864128e1ab75e2396dd7c94f43a9beb0a
paulacarvani <3312@holbertonschool.com>
2021-06-18 20:52:03 -0700

    Program to prints numbers in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int base16 = 48;
+while (base16 <= 102)
+{
+if (base16 <= 57 && base16 >= 97)
+
+putchar (base16);
+base16++;
+}
+{
+putchar ('\n');
+}
+return (0);
+}
