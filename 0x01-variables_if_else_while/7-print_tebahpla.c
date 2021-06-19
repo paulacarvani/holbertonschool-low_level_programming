1e8c11dce0730e209e60222229d2157921948e0b
paulacarvani <3312@holbertonschool.com>
2021-06-18 20:27:57 -0700

    New program for prints the alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int tebahpla = 122;
+while (tebahpla >= 97)
+{
+putchar (tebahpla);
+tebahpla--;
+}
+{
+putchar ('\n');
+}
+return (0);
+}
