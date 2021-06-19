bac203a30fe04346ea5a800d1438c02adc3f5518
paulacarvani <3312@holbertonschool.com>
2021-06-18 18:49:25 -0700

    Creates a script to use putchar 3 times

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int alphabet = 97;
+
+while (alphabet <= 122)
+
+{
+putchar (alphabet);
+alphabet++;
+}
+int alphabets = 65;
+
+while (alphabets <= 90)
+{
+putchar (alphabets);
+alphabet++;
+}
+{
+putchar('\n');
+}
+return (0);
+}
