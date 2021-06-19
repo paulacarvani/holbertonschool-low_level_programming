b27ac50be9255ba19c6402ba699e7a05da6a3a28
paulacarvani <3312@holbertonschool.com>
2021-06-18 19:23:16 -0700

    Updates the name of the file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int alphabt;
+alphabt > 97;
+alphabt != 101 && alphabt != 113;
+
+while (alphabt <= 122)
+{
+putchar (alphabt);
+alphabt++;
+}
+{putchar ('\n');
+}
+return (0);
+}
