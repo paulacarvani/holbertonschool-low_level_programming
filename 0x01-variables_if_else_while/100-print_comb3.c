b84fb4b3ccf48e5ca9519759f2ec7b8d0642da3e
paulacarvani <3312@holbertonschool.com>
2021-06-21 09:27:20 -0700

    Updates to betty style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,25 @@
  */
 int main(void)
 {
-  int a = 48;
-  while (a < 57)
-    {
-      int c = 0;
-      while (0 <= 1)
-	{
-	  int b = 48;
-	  if (a != b)
-	    while (b <= 57)
-	  {
-	    putchar (b);
-	    b++;
-	  }
-	  c++;
-	}
-      putchar (a);
-      putchar (',');
-      putchar (' ');
-      a++;
-    }
-  return (0);
+int a = 48;
+while (a < 57)
+{
+int c = 0;
+while (0 <= 1)
+{
+int b = 48;
+if (a != b)
+while (b <= 57)
+{
+putchar (b);
+b++;
+}
+c++;
+}
+putchar (a);
+putchar (',');
+putchar (' ');
+a++;
+}
+return (0);
 }
