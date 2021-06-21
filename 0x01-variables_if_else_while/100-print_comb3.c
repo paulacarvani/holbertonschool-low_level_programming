6cfbd55c014497529b5106447df7b0f9bc1e867b
paulacarvani <3312@holbertonschool.com>
2021-06-20 23:23:37 -0700

    New progrma of a loop into a loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+  int a ='0';
+  int b = '0';
+
+    while (a <= '9')
+      {
+       if (a != b)
+	 {
+	   while (b <= '9')
+	   
+	   putchar (b);
+	   b++;
+	   
+       
+	 putchar (a);
+       a++;
+	 
+    putchar (',');
+    putchar (' ');
+      }
+      }
+    
+    return (0);
+}
