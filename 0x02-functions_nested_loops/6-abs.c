1ddb56cb84f25021d72da782400013c48e88e77e
paulacarvani <3312@holbertonschool.com>
2021-06-21 22:04:13 -0700

    Upload all the files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int)
+{
+  if (int >= '0')
+    int * '1';
+  else
+    int * '-1';
+}
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_alphabet(void);
+int _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+
+
+
+  #endif
