6d79de90c6410cf8319121897c8829721459abb0
paulacarvani <3312@holbertonschool.com>
2021-06-21 22:19:48 -0700

    Changing the betty style

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,22 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * _isalpha - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 
 int _isalpha(int c)
 {
-  if(c >= 97 && c <= 122)
-    {
-      return (1);
-    }
-  else if (c >= 65 && c <= 90)
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+if (c >= 97 && c <= 122)
+{
+return (1);
 }
-	   
+else if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
