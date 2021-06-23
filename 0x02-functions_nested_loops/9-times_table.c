fe76d2ae4c8ae0aa885ce6d0ade16b93d1ddfda6
paulacarvani <3312@holbertonschool.com>
2021-06-22 18:47:55 -0700

    Update the betty style

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,30 +13,28 @@ int j;
 int k;
 for (i = 0; i <= 9; i++)
 {
-  for (j = 0; j <= 9; j++)
-    {
-      k = i * j;
-      if (j == 0)
-	{
-	  _putchar(k + '0');
-	}
-      else if (k <= 9)
-	{
-	  _putchar(',');
-	  _putchar(' ');
-	  _putchar(' ');
-	  _putchar(k + '0');
-	}
-      else if (k >= 10)
-	{
-	  _putchar(',');
-	  _putchar(' ');
-	  _putchar(k / 10 + '0');
-	  _putchar(k % 10 + '0');
-	}
-    }
-  _putchar('\n');
- }
+for (j = 0; j <= 9; j++)
+{
+k = i * j;
+if (j == 0)
+{
+_putchar(k + '0');
 }
-	
-  
+else if (k <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
