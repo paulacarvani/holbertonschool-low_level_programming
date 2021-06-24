c0f3ddc636ac06e1652c9559127b5f7bcfdf3345
paulacarvani <3312@holbertonschool.com>
2021-06-24 12:26:15 -0700

    Updates holberton and make a program that returns a product of two var

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *mul - Prototype that returns the product of two variables
+ *@a: first var
+ *@b: second var
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
