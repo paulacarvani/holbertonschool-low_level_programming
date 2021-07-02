b5090f288a25e0172c99d299b114d8597c04c5a0
paulacarvani <3312@holbertonschool.com>
2021-07-02 12:44:58 -0700

    Updates betty style

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
- * reverse_array - function that reverses the content of an array of integers.
+ * reverse_array - function that reverses the content of an array
  *@a: variable type prototype
- *@b: variable type int
+ *@n: variable type int
 * Return: nothing.
  */
 void reverse_array(int *a, int n)
 {
 int i, j, k;
 k = 0;
-for (i = n - 1; i >= (n /2); i--)
+for (i = n - 1; i >= (n / 2); i--)
 {
 j = a[k];
 a[k] = a[i];
