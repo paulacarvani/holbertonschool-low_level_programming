3382158c1e4b04f33c866e8bdd369093720fae49
paulacarvani <3312@holbertonschool.com>
2021-07-14 12:35:02 -0700

    Update the betty style

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,23 @@
 #include "holberton.h"
 /**
- * _strdup - function that returns a pointer to a newly allocated space in memory
+ * _strdup - returns a pointer to a newly allocated space in memory
  *@str: string
  *Return: Depends of the result
  */
 char *_strdup(char *str)
 {
-  char *c;
-    unsigned int i;
-  unsigned int j;
-  if (str == NULL)
-    return (NULL);
-  for (i = 0; str[i]; i++)
-    ;
-  c = malloc(i + 1);
-  if (c == NULL)
-    return (NULL);
-  for (j = 0; str[j]; j++)
-    c[j] = str[j];
-  c[j] = '\0';
-  return (c);
+char *c;
+unsigned int i;
+unsigned int j;
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i]; i++)
+;
+c = malloc(i + 1);
+if (c == NULL)
+return (NULL);
+for (j = 0; str[j]; j++)
+c[j] = str[j];
+c[j] = '\0';
+return (c);
 }
