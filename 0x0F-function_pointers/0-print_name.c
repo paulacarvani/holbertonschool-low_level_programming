9a0e00400e0eadd6305e0f5d5d9ba39a3624a443
paulacarvani <3312@holbertonschool.com>
2021-07-21 15:06:52 -0700

    Updating the function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,6 @@
  *print_name - function that prints a name
  *@name: pointer to the var name
  *@f: pointer to function
- *Return: Prints a name
  */
 
 void print_name(char *name, void (*f)(char *))
