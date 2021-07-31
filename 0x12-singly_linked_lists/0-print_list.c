be78a34fac7713f0665895923dcc8558fa47551d
paulacarvani <3312@holbertonschool.com>
2021-07-30 19:22:53 -0700

    New update of the function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,11 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t nodes = 0;
-while (h)
+if (h == NULL)
 {
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
-nodes++;
-h = h->next;
+return (0);
 }
-return (nodes);
+printf("[%u] %s\n", h->str ? h->len : 0,
+h->str ? h->str : "(nil)");
+return (1 + print_list(h->next));
 }
