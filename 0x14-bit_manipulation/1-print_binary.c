f52c3e8d256a04339a5f4b0eec4e02a836868e24
paulacarvani <3312@holbertonschool.com>
2021-08-05 15:26:26 -0700

    Update the function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,18 +6,9 @@
  */
 void print_binary(unsigned long int n)
 {
-if (n == 0)
-{
-_putchar('0');
-}
-if (n == 1)
-{
-_putchar('1');
-return;
-}
-if (n > 1)
-{
-print_binary(n >> 1);
-printf("%lu", n & 1);
-}
+  if (n > 1)
+    {
+      print_binary (n >> 1);
+    }
+  _putchar((n & 1) + '0');
 }
