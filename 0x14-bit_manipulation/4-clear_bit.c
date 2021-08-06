40afb9cca6d1de8d8b7c2df0fca85afc30b65c89
paulacarvani <3312@holbertonschool.com>
2021-08-05 21:30:25 -0700

    Task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main."
+
+/**
+ * clear_bit - clear
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: cleared
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+  unsigned long int ms;
+
+  ms = (sizeof(unsigned long int) * 8 - 1);
+  if (index > ms)
+    {
+      return (-1);
+    }
+  *n = *n & ~(1ul << index);
+  return (1);
+}
