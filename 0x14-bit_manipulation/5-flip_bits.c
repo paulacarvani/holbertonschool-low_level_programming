2d737f966801e108c168b150071af72d3307ce74
paulacarvani <3312@holbertonschool.com>
2021-08-05 21:56:41 -0700

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,16 +7,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-  unsigned int count = 0;
-
-  while (n != 0 || m != 0)
-    {
-      if ((n & 1) != (m & 1))
-	{
-	  count++;
-	}
-      m = m >> 1;
-      n = n >> 1;
-    }
-  return (count);
+unsigned int count = 0;
+while (n != 0 || m != 0)
+{
+if ((n & 1) != (m & 1))
+{
+count++;
+}
+m = m >> 1;
+n = n >> 1;
+}
+return (count);
 }
