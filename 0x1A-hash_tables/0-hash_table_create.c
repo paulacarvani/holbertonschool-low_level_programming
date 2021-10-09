37083227270e70fbf75b7222ebb086f5aa9f4e4b
paulacarvani <3312@holbertonschool.com>
2021-10-09 18:26:22 -0500

    First files, task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hast table
+ * @size: size of array
+ * Return: New Hash_table or if is error NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *nht;
+    unsigned long int i;
+
+    if (size == 0)
+        return (NULL);
+
+    nht = malloc(sizeof(hash_table_t));
+    if (nht == NULL)
+        return (NULL);
+
+    nht->array = malloc(sizeof(hash_table_t *) * size);
+    if (nht->array == NULL)
+    {
+        free(nht);
+        return (NULL);
+    }
+    nht->size = size;
+    for (i = 0; i< size; i++)
+        nht->array[i] = NULL;
+
+    return (nht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+# 0x1A-hash_tables
+
+Project to create a Hash table and Hash function
